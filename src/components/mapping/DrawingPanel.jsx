e531d92b7e3815e3f805f910a9b28a1b00e62b9a
Nafisa Anzum <anzumnafisa13@gmail.com>
2021-08-17 16:03:38 -0400

    graph panel added

--- src/components/mapping/DrawingPanel.jsx
+++ src/components/mapping/DrawingPanel.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+
+import GraphPanel2 from "./GraphPanel2";
 class DrawingPanel extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+
+      nodes:
+        [
+        ],
+      links: 
+        [
+        ]
+  
+    }
+      
     
   }
 
@@ -18,6 +32,7 @@ class DrawingPanel extends Component {
         onDrop={(e)=>this.onDrop(e)}>
           <span className="pull-right logo-color"> Drawing panel</span>
 
+        <GraphPanel2 nodes={this.state.nodes} links={this.state.links} addEdge={this.addEdge}/>
         </div>
       </>
     );
@@ -27,19 +42,45 @@ class DrawingPanel extends Component {
     ev.preventDefault();
   };
 
+  getNodeObject = (table, id) => {
+    return {
+      id: id,
+      name: table.name,
+      table: table
+    }
+  }
+
+  addEdge = (source, target) => {
+    console.log("Souce", source);
+    console.log("target", target);
+    let newLink ={ source: source, target: target, left : false, right : true };
+    this.setState(prevState => ({
+      links: [...prevState.links, newLink]
+    }));
+    let action = {
+      type : 2,
+      link : newLink,
+      description : newLink.source.name +"-"+ newLink.target.name +" edge type created."
+    }
+    this.props.onChangeGraph(action, {nodes: this.state.nodes, edges: this.state.edges})
+  }
+
   onDrop = (ev) => {
     let id = ev.dataTransfer.getData("tableId");
     console.log("dropped Table ID", id);
-    let newNode = this.props.tables[id];
-    console.log("dropped Table", newNode);
+    let newNode = this.getNodeObject(this.props.tables[id], this.state.nodes.length);
+    console.log("dropped node", newNode);
     this.graph.nodes.push(newNode);
+    this.setState(prevState => ({
+      nodes: [...prevState.nodes, newNode]
+    }));
     let action = {
       id:0,
       type : 1,
       node : newNode,
       description : newNode.name+" node type created."
     }
-    this.props.onChangeGraph(action, this.graph)
+    this.props.onChangeGraph(action, {nodes: this.state.nodes, edges: this.state.edges})
   }
 }
 
